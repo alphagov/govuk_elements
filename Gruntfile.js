20395590dae8d54dd23c9a1ab616f9fb9491544f
Gemma Leigh <hello@gemmaleigh.com>
2014-03-25 12:48:21 +0000

    Add support for old IEs
    - Add stylesheets for IE 6 - 8
    - Rename ‘examples’ to ‘elements-page’ to make it clearer what this
    stylesheet is for
    - Ensure example templates (grid, typography, forms) only load
    ‘main.css’, not the elements page styles.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,13 @@ module.exports = function(grunt){
       ruby: {
         files: {
           'public/stylesheets/main.css': 'public/sass/main.scss',
-          'public/stylesheets/examples.css': 'public/sass/examples.scss'
+          'public/stylesheets/main-ie6.css': 'public/sass/main-ie6.scss',
+          'public/stylesheets/main-ie7.css': 'public/sass/main-ie7.scss',
+          'public/stylesheets/main-ie8.css': 'public/sass/main-ie8.scss',
+          'public/stylesheets/elements-page.css': 'public/sass/elements-page.scss',
+          'public/stylesheets/elements-page-ie6.css': 'public/sass/elements-page-ie6.scss',
+          'public/stylesheets/elements-page-ie7.css': 'public/sass/elements-page-ie7.scss',
+          'public/stylesheets/elements-page-ie8.css': 'public/sass/elements-page-ie8.scss'
         },
         options: {
           loadPath: ['govuk/public/sass'],
