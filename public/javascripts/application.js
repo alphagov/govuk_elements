7b1a44ec413c666290b4cbcc2baf64ee0c5811df
Gemma Leigh <hello@gemmaleigh.com>
2014-03-31 14:45:23 +0100

    Add toggle to indented form example

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,31 +1,31 @@
 $(document).ready(function () {
-    
+
   // Example - Highlight grid
 
   if ($('.js-highlight-grid').length>0) {
-    
+
     $('.js-highlight-grid').click(function(e) {
 
       e.preventDefault();
       var html = $('html');
-      
+
       if ($('.is-inner-block-highlight').length>0) {
         // Don't add more than once
       } else {
         $('.grid .inner-block').wrapInner('<div class="is-inner-block-highlight"></div>');
       }
-      
+
       if (html.hasClass('example-highlight-grid')) {
           html.removeClass('example-highlight-grid');
       } else {
           html.addClass('example-highlight-grid');
       }
-      
+
     });
   }
 
   // Example - Form focus styles
-  
+
   if ($('.form').length>0) {
 
     $(".block-label").each(function(){
@@ -38,14 +38,24 @@ $(document).ready(function () {
       });
       // Add selected class
       $('input:checked').parent().addClass('selected');
+
     });
 
-    // Remove selected class
+    // Add/remove selected class
     $('.block-label').find('input[type=radio], input[type=checkbox]').click(function() {
+
       $('input:not(:checked)').parent().removeClass('selected');
       $('input:checked').parent().addClass('selected');
-    });
 
+      // Hide open data-toggle content
+      $('.toggle-content').hide();
+
+      // Show data-toggle content
+      var target = $(this).parent().attr('data-target');
+      $('#'+target).show();
+
+    });
+    
   }
-  
+
 });
