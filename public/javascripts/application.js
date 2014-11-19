428add1a316dae3592baf3b177ff2565d2d99343
Gemma Leigh <gemma@little-digital.com>
2014-11-19 10:13:38 +0000

    Update SelectionButtons to new interface

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -113,8 +113,7 @@ $(document).ready(function() {
   // Use GOV.UK selection-buttons.js to set selected
   // and focused states for block labels
   var $blockLabels = $(".block-label input[type='radio'], .block-label input[type='checkbox']");
-
-  GOVUK.selectionButtons($blockLabels);
+  new GOVUK.SelectionButtons($blockLabels);
 
   // Details/summary polyfill
   // See /javascripts/vendor/details.polyfill.js
