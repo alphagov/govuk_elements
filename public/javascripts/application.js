2ae729b3047e8cb2540eef9670b11e6765e9768e
Gemma Leigh <gemma@little-digital.com>
2015-04-09 16:00:36 +0100

    Move setting focus to within window load

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -113,29 +113,6 @@ function ShowHideContent() {
   }
 }
 
-function setFocus() {
-  var self = this;
-  self.exampleSetFocus = function () {
-
-    // Only set focus for the error example pages
-    if ($(".js-error-example").length) {
-      // If there is an error summary, set focus to the summary
-      if ($(".error-summary").length) {
-        $(".error-summary").focus();
-        $(".error-summary a").click(function(e) {
-          e.preventDefault();
-          var href = $(this).attr("href");
-          $(href).focus();
-        });
-      }
-      // Otherwise, set focus to the field with the error
-      else {
-        $(".error input:first").focus();
-      }
-    }
-  }
-}
-
 $(document).ready(function() {
 
   // Turn off jQuery animation
@@ -154,10 +131,27 @@ $(document).ready(function() {
   var toggleContent = new ShowHideContent();
   toggleContent.showHideRadioToggledContent();
   toggleContent.showHideCheckboxToggledContent();
-
-  // $(window).load(function() { 
-  //   var focusContent = new setFocus();
-  //   focusContent.exampleSetFocus();
-  // });
   
+});
+
+$(window).load(function() { 
+
+  // Only set focus for the error example pages
+  if ($(".js-error-example").length) {
+    
+    // If there is an error summary, set focus to the summary
+    if ($(".error-summary").length) {
+      $(".error-summary").focus();
+      $(".error-summary a").click(function(e) {
+        e.preventDefault();
+        var href = $(this).attr("href");
+        $(href).focus();
+      });
+    }
+    // Otherwise, set focus to the field with the error
+    else {
+      $(".error input:first").focus();
+    }
+  }
+
 });
