612caeaf2b19013bdc5caad1ade26663c126aab7
Gemma Leigh <hello@gemmaleigh.com>
2015-05-14 11:50:59 +0100

    Set focus for error example pages
    If there is an error summary, set focus to the summary - otherwise,
    set focus to the first input (to cover both text input and radios or
    checkboxes)

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -132,3 +132,26 @@ $(document).ready(function() {
   toggleContent.showHideCheckboxToggledContent();
 
 });
+
+$(window).load(function() {
+
+  // Only set focus for the error example pages
+  if ($(".js-error-example").length) {
+
+    // If there is an error summary, set focus to the summary
+    if ($(".error-summary").length) {
+      $(".error-summary").focus();
+      $(".error-summary a").click(function(e) {
+        e.preventDefault();
+        var href = $(this).attr("href");
+        $(href).focus();
+      });
+    }
+    // Otherwise, set focus to the field with the error
+    else {
+      $(".error input:first").focus();
+    }
+  }
+
+});
+
