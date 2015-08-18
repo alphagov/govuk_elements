991d66d621fbfb59a30b34cb53be124ce8408f85
Gemma Leigh <gemma@little-digital.com>
2015-08-18 11:29:33 +0100

    Bump govuk template to 0.14.3
    # 0.14.3
    
    - Fix jumping logo on hover
    
    # 0.14.2
    
    - Drop Windows Safari font printing workaround as the font is no longer
    used in print
    - Update the logo to print black-on-white instead of white-on-black

--- govuk/public/javascripts/govuk-template.js
+++ govuk/public/javascripts/govuk-template.js
@@ -77,20 +77,6 @@
 (function() {
   "use strict"
 
-  // fix for printing bug in Windows Safari
-  var windowsSafari = (window.navigator.userAgent.match(/(\(Windows[\s\w\.]+\))[\/\(\s\w\.\,\)]+(Version\/[\d\.]+)\s(Safari\/[\d\.]+)/) !== null),
-      style;
-
-  if (windowsSafari) {
-    // set the New Transport font to Arial for printing
-    style = document.createElement('style');
-    style.setAttribute('type', 'text/css');
-    style.setAttribute('media', 'print');
-    style.innerHTML = '@font-face { font-family: nta !important; src: local("Arial") !important; }';
-    document.getElementsByTagName('head')[0].appendChild(style);
-  }
-
-
   // add cookie message
   if (window.GOVUK && GOVUK.addCookieMessage) {
     GOVUK.addCookieMessage();
