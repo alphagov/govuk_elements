e325c7a877275184d333aa233af1280d380cc5fa
Gemma Leigh <hello@gemmaleigh.com>
2015-08-28 16:13:15 +0100

    Add breadcrumb
    - Set breadcrumb to show section, section name and page name
    - Rename breadcrumb
    - Update pages using the breadcrumb include

--- routes.js
+++ routes.js
@@ -1,36 +1,6 @@
 module.exports = {
   bind : function (app, assetPath) {
 
-    app.use(function (req, res, next) {
-
-      // Get the path
-      var path = req.path;
-
-      // Replace any slash at the end of a line with nothing
-      var path_parts = path.replace(/\/\s*$/, '').split('/');
-
-      if (path_parts.length === "3") {
-
-        var section = path_parts[1];
-        var page = path_parts[2];
-
-        res.locals({
-          "section": section,
-          "page": page
-        });
-
-
-      } else if (path_parts.length === "2") {
-
-        var page = path_parts[1];
-
-        res.locals({
-          "page": page
-        });
-      }
-      next();
-    });
-
     app.get('/', function (req, res) {
       res.render('index', {'assetPath' : assetPath });
     });
@@ -43,9 +13,10 @@ module.exports = {
 
     // Example page: Grid layout
     app.get('/layout/example-grid-layout', function (req, res) {
+      var section = "layout";
       var section_name = "Layout";
       var page_name = "Example: Grid layout";
-      res.render('examples/example_grid_layout', {'assetPath' : assetPath, 'section_name' : section_name, 'page_name' : page_name });
+      res.render('examples/example_grid_layout', {'assetPath' : assetPath, 'section': section, 'section_name' : section_name, 'page_name' : page_name });
     });
 
     // Typography
@@ -56,16 +27,18 @@ module.exports = {
 
     // Example page: Typography
     app.get('/typography/example-typography', function (req, res) {
+      var section = "typography";
       var section_name = "Typography";
       var page_name = "Example: Typography";
-      res.render('examples/example_typography', {'assetPath' : assetPath, 'section_name' : section_name, 'page_name' : page_name });
+      res.render('examples/example_typography', {'assetPath' : assetPath, 'section': section, 'section_name' : section_name, 'page_name' : page_name });
     });
 
     // Example page: Progressive disclosure
     app.get('/typography/example-details-summary', function (req, res) {
+      var section = "typography";
       var section_name = "Typography";
       var page_name = "Example: Details summary";
-      res.render('examples/example_details_summary', {'assetPath' : assetPath, 'section_name' : section_name, 'page_name' : page_name });
+      res.render('examples/example_details_summary', {'assetPath' : assetPath, 'section': section, 'section_name' : section_name, 'page_name' : page_name });
     });
 
     // Colour
@@ -100,30 +73,34 @@ module.exports = {
 
     // Example page: Basic form
     app.get('/forms/example-forms', function (req, res) {
+      var section = "forms";
       var section_name = "Forms";
-      var page_name = "Example: Forms";
-      res.render('examples/example_forms', {'assetPath' : assetPath, 'section_name' : section_name, 'page_name' : page_name });
+      var page_name = "Example: Form";
+      res.render('examples/example_forms', {'assetPath' : assetPath, 'section': section, 'section_name' : section_name, 'page_name' : page_name });
     });
 
     // Example page: Date pattern
-    app.get('/forms/date', function (req, res) {
+    app.get('/forms/example-date', function (req, res) {
+      var section = "forms";
       var section_name = "Forms";
       var page_name = "Example: Date";
-      res.render('examples/example_date', {'assetPath' : assetPath, 'section_name' : section_name, 'page_name' : page_name });
+      res.render('examples/example_date', {'assetPath' : assetPath, 'section': section, 'section_name' : section_name, 'page_name' : page_name });
     });
 
     // Example page: Radio buttons and checkboxes
-    app.get('/forms/radios-checkboxes', function (req, res) {
+    app.get('/forms/example-radios-checkboxes', function (req, res) {
+      var section = "forms";
       var section_name = "Forms";
       var page_name = "Example: Radio buttons and checkboxes";
-      res.render('examples/example_radios_checkboxes', {'assetPath' : assetPath, 'page_name' : page_name, 'section_name' : section_name });
+      res.render('examples/example_radios_checkboxes', {'assetPath' : assetPath, 'section': section, 'section_name' : section_name, 'page_name' : page_name });
     });
 
     // Example page: Form elements
-    app.get('/forms/form-elements', function (req, res) {
+    app.get('/forms/example-form-elements', function (req, res) {
+      var section = "forms";
       var section_name = "Forms";
       var page_name = "Example: Form elements";
-      res.render('examples/example_form_elements', {'assetPath' : assetPath, 'section_name' : section_name, 'page_name' : page_name });
+      res.render('examples/example_form_elements', {'assetPath' : assetPath, 'section': section, 'section_name' : section_name, 'page_name' : page_name });
     });
 
     // Errors and validation
@@ -134,13 +111,15 @@ module.exports = {
 
     // Example page: Form validation
     app.get('/errors/example-form-validation-single-question-radio', function (req, res) {
-      var section_name = "Errors";
+      var section = "errors";
+      var section_name = "Errors and validation";
       var page_name = "Example: Form validation - single question";
-      res.render('examples/example_form_validation_single_question_radio', {'assetPath' : assetPath, 'section_name' : section_name, 'page_name' : page_name });
+      res.render('examples/example_form_validation_single_question_radio', {'assetPath' : assetPath, 'section': section, 'section_name' : section_name, 'page_name' : page_name });
     });
 
     app.post('/errors/example-form-validation-single-question-radio', function (req, res) {
-      var section_name = "Forms";
+      var section = "errors";
+      var section_name = "Errors and validation";
       var page_name = "Example: Form validation - single question";
       var personal_details = req.body.personal_details;
       var error = false;
@@ -149,16 +128,18 @@ module.exports = {
       } else {
         error = false;
       }
-      res.render('examples/example_form_validation_single_question_radio', {'assetPath' : assetPath, 'section_name' : section_name, 'page_name' : page_name, 'personal_details': personal_details, 'error': error});
+      res.render('examples/example_form_validation_single_question_radio', {'assetPath' : assetPath, 'section': section, 'section_name' : section_name, 'page_name' : page_name, 'personal_details': personal_details, 'error': error});
     });
 
     app.get('/errors/example-form-validation-multiple-questions', function (req, res) {
+      var section = "errors";
       var section_name = "Errors and validation";
       var page_name = "Example: Form validation - multiple questions";
-      res.render('examples/example_form_validation_multiple_questions', {'assetPath' : assetPath, 'section_name' : section_name, 'page_name' : page_name });
+      res.render('examples/example_form_validation_multiple_questions', {'assetPath' : assetPath, 'section': section, 'section_name' : section_name, 'page_name' : page_name });
     });
 
     app.post('/errors/example-form-validation-multiple-questions', function (req, res) {
+      var section = "errors";
       var section_name = "Errors and validation";
       var page_name = "Example: Form validation - multiple questions";
       var fullName = req.body.fullName;
@@ -169,11 +150,11 @@ module.exports = {
       } else {
         error = false;
       }
-      res.render('examples/example_form_validation_multiple_questions', {'assetPath' : assetPath, 'section_name' : section_name, 'page_name' : page_name, 'fullName': fullName, 'niNo': niNo, 'error': error});
+      res.render('examples/example_form_validation_multiple_questions', {'assetPath' : assetPath, 'section': section, 'section_name' : section_name, 'page_name' : page_name, 'fullName': fullName, 'niNo': niNo, 'error': error});
     });
 
     // Alpha and beta banners
-    app.get('/alpha-beta', function (req, res) {
+    app.get('/alpha-beta-banners', function (req, res) {
       var page_name = "Alpha and beta banners";
       res.render('guide_alpha_beta', {'assetPath' : assetPath, 'page_name' : page_name });
     });
