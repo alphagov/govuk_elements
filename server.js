a48fdbb4bdd5e485bc76caf10a4ce0c3df825f17
Joe Lanman <joelanman@gmail.com>
2016-06-13 13:40:04 +0100

    express 4

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-var express = require('express'),
+var bodyParser = require('body-parser'),
+	express = require('express'),
     routes = require(__dirname + '/app/routes.js'),
+    
     app = express(),
     port = (process.env.PORT || 3000);
 
@@ -12,8 +14,12 @@ app.set('views', __dirname + '/app/views');
 // Middleware to serve static assets
 app.use('/public', express.static(__dirname + '/public'));
 app.use('/public', express.static(__dirname + '/govuk_modules/public'));
-app.use(express.json());       // to support JSON-encoded bodies
-app.use(express.urlencoded()); // to support URL-encoded bodies
+
+// Support for parsing data in POSTs
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
 // routes (found in routes.js)
 
