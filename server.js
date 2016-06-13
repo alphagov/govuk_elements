07927dc8aee58a9f7df5ffc2d031ecd0a645bdb9
Joe Lanman <joelanman@gmail.com>
2016-06-13 12:30:13 +0100

    move files to /app/ and use server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express'),
-    routes = require(__dirname + '/routes.js'),
+    routes = require(__dirname + '/app/routes.js'),
     app = express(),
     port = (process.env.PORT || 3000);
 
@@ -7,7 +7,7 @@ var express = require('express'),
 app.engine('html', require(__dirname + '/lib/template-engine.js').__express);
 app.set('view engine', 'html');
 app.set('vendorViews', __dirname + '/govuk_modules/views');
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/app/views');
 
 // Middleware to serve static assets
 app.use('/public', express.static(__dirname + '/public'));
