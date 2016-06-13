69e9c6e5c00fe66906e25f2aaf6ebe7890691f4f
Gemma Leigh <hello@gemmaleigh.com>
2016-06-13 13:34:59 +0100

    Copy assets to public and consolidate govuk copy task

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,47 +26,28 @@ module.exports = function (grunt) {
 
     // Copies templates and assets from external modules and dirs
     copy: {
-
-      govuk_template: {
-        src: 'node_modules/govuk_template_mustache/views/layouts/govuk_template.html',
-        dest: 'govuk_modules/views/',
-        expand: true,
-        flatten: true,
-        filter: 'isFile'
+      assets: {
+        files: [{
+          expand: true,
+          cwd: 'app/assets/',
+          src: ['**/*', '!sass/**'],
+          dest: 'public/'
+        }]
       },
-
-      govuk_assets: {
-        files: [
-          {
-            expand: true,
-            src: '**',
-            cwd: 'node_modules/govuk_template_mustache/assets',
-            dest: 'govuk_modules/public/'
-          }
-        ]
+      govuk: {
+        files: [{
+          expand: true,
+          cwd: 'node_modules/govuk_frontend_toolkit/govuk_frontend_toolkit',
+          src: '**',
+          dest: 'govuk_modules/govuk_frontend_toolkit/'
+        },
+        {
+          expand: true,
+          cwd: 'node_modules/govuk_template_mustache/',
+          src: '**',
+          dest: 'govuk_modules/govuk_template/'
+        }]
       },
-
-      govuk_frontend_toolkit_scss: {
-        expand: true,
-        src: '**',
-        cwd: 'node_modules/govuk_frontend_toolkit/stylesheets/',
-        dest: 'govuk_modules/public/sass/'
-      },
-
-      govuk_frontend_toolkit_js: {
-        expand: true,
-        src: '**',
-        cwd: 'node_modules/govuk_frontend_toolkit/javascripts/',
-        dest: 'govuk_modules/public/javascripts/'
-      },
-
-      govuk_frontend_toolkit_img: {
-        expand: true,
-        src: '**',
-        cwd: 'node_modules/govuk_frontend_toolkit/images/',
-        dest: 'govuk_modules/public/images/'
-      },
-
     },
 
     // workaround for libsass
@@ -145,12 +126,8 @@ module.exports = function (grunt) {
   );
 
   grunt.registerTask('default', [
-    'copy:govuk_template',
-    'copy:govuk_assets',
+    'copy',
     'convert_template',
-    'copy:govuk_frontend_toolkit_scss',
-    'copy:govuk_frontend_toolkit_js',
-    'copy:govuk_frontend_toolkit_img',
     'replace',
     'sass',
     'concurrent:target'
@@ -160,12 +137,8 @@ module.exports = function (grunt) {
     'test_default',
     'Test that the default task runs the app',
     [
-      'copy:govuk_template',
-      'copy:govuk_assets',
+      'copy',
       'convert_template',
-      'copy:govuk_frontend_toolkit_scss',
-      'copy:govuk_frontend_toolkit_js',
-      'copy:govuk_frontend_toolkit_img',
       'replace',
       'sass'
     ]
