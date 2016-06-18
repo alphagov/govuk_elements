6844902591e5e2803092c436806dc6cce83e4345
Gemma Leigh <hello@gemmaleigh.com>
2016-06-18 15:45:31 +0100

    Add a config file for the app
    Use this to define a variable for the cookieText

---
+++ app/config.js
@@ -0,0 +1,10 @@
+// Use this file to change prototype configuration.
+
+// Note: this config can be overridden using environment variables (eg on heroku)
+
+module.exports = {
+
+  // Cookie warning
+  cookieText: 'GOV.UK uses cookies to make the site simpler. <a href="https://www.gov.uk/help/cookies" title="Find out more about cookies">Find out more about cookies</a>'
+
+};
