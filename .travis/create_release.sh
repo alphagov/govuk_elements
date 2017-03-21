f3dd1e17d031c1865a336e0e9fef41f737345716
Tom Byers <tombaromba@gmail.com>
2017-03-21 15:28:36 +0000

    Move release creation into separate script

---
+++ .travis/create_release.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+set -e
+
+echo "Starting provider"
+
+git config --global user.name "Travis CI"
+git config --global user.email "travis@travis-ci.org"
+git remote add origin_ssh git@github.com:alphagov/govuk_elements.git
+openssl aes-256-cbc -K $encrypted_85ebe8034b89_key -iv $encrypted_85ebe8034b89_iv -in .travis/govuk_elements.enc -out ~/.ssh/id_rsa -d && chmod 600 ~/.ssh/id_rsa # This openssl command was generated automatically by `travis encrypt-file`, see `.travis/README.md` for more details
+ls -al ~/.ssh
+head -1 ~/.ssh/id_rsa
+tail -1 ~/.ssh/id_rsa
+ssh git@github.com || true
+
+./push-version-tag.sh
+./update-release-branch.sh
