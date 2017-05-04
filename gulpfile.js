f964b185c3809b0ea4503f4665335b820cc458a2
Gemma Leigh <hello@gemmaleigh.com>
2017-05-04 18:30:16 +0100

    Add an Images build task
    Copies images from assets/ to /public/images

--- gulpfile.js
+++ gulpfile.js
@@ -32,3 +32,13 @@ gulp.task('styles', () => {
     .pipe(cssnano())
     .pipe(gulp.dest(paths.publicCss))
 })
+
+// Images build task ---------------------
+// Copies images to /public/images
+// ---------------------------------------
+
+gulp.task('images', () => {
+  return gulp.src(paths.assetsImg + '**/*')
+    .pipe(gulp.dest(paths.publicImg))
+})
+
