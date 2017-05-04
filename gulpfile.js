cbfd27df9724931ed5fa31971b0fc68515d066e8
Gemma Leigh <hello@gemmaleigh.com>
2017-05-04 18:28:07 +0100

    Add a Styles build task
    Compiles CSS from Sass
    Output both a minified and non-minified version into
    /public/stylesheets.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@
 const paths = require('./config/paths.json')
 const gulp = require('gulp')
 const del = require('del')
+const rename = require('gulp-rename')
+const sass = require('gulp-sass')
 
 // Clean task ----------------------------
 // Deletes the /public directory
@@ -12,3 +14,19 @@ gulp.task('clean', () => {
   return del(paths.public)
 })
 
+// Styles build task ---------------------
+// Compiles CSS from Sass
+// Output both a minified and non-minified version into /public/stylesheets/
+// ---------------------------------------
+
+gulp.task('styles', () => {
+  return gulp.src(paths.assetsScss + '**/*.scss')
+    .pipe(sass({
+      includePaths: [
+        'node_modules/govuk_frontend_toolkit/stylesheets'
+      ]
+    }).on('error', sass.logError))
+    .pipe(gulp.dest(paths.publicCss))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(paths.publicCss))
+})
