f3678037140dc8efdb70e643bda51fc0160f05d3
Gemma Leigh <hello@gemmaleigh.com>
2017-07-14 11:46:39 +0100

    Add a check to see if the tag for a version exists
    If so, proceed to create release, otherwise exit.

--- scripts/check-release-version.sh
+++ scripts/check-release-version.sh
@@ -1,8 +1,16 @@
-echo "Check if VERSION.txt has been updated"
+echo "Check to see if a tag for this version exists"
 
-if git diff --exit-code --quiet packages/govuk-elements-sass/VERSION.txt; then
-  echo "VERSION.txt is unchanged, no need to create release."
-  exit 1
-else
+# Get the version from the version file
+VERSION_TAG="v`cat packages/govuk-elements-sass/VERSION.txt`"
+
+# Create a new tag - if the version file has been updated and a tag for that
+# version doesn't already exist
+
+# Check to make sure the tag doesn't already exist
+if ! git rev-parse $VERSION_TAG >/dev/null 2>&1; then
+  echo "A tag for this version doesn't already exist"
   echo "Proceed to create release..."
+else
+  echo "A tag for this version exists, no need to create release."
+  exit 1
 fi
