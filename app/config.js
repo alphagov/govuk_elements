2def70c7250122e465ded40194b36ddd27d90a4c
Nick Colley <nickcolley7@gmail.com>
2019-12-20 11:13:44 +0000

    Remove cookie message

--- app/config.js
+++ app/config.js
@@ -2,9 +2,4 @@
 
 // Note: this config can be overridden using environment variables (eg on heroku)
 
-module.exports = {
-
-  // Cookie warning
-  cookieText: 'GOV.UK uses cookies to make the site simpler. <a href="https://www.gov.uk/help/cookies">Find out more about cookies</a>'
-
-}
+module.exports = { }
